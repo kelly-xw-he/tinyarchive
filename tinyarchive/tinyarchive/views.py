765b6549f0b13b608cc0f9140d3ef11551a1691c
Kelly He <kellyhe@Kellys-Air.fios-router.home>
2023-08-06 20:31:03 -0400

    Project Code

--- tinyarchive/tinyarchive/views.py
+++ tinyarchive/tinyarchive/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # This is just the view for the static homepage.
 def home(request):
     return render(request, "home.html", {})
+def aboutus(request):
+    return render(request, "aboutus.html", {})
